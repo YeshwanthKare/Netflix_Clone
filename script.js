9c5a8f71b33471336dc4d6afdddac4bf8a371392
YeshwanthKare <yesh.kare@gmail.com>
2021-01-24 17:14:00 +0100

    add function for fetching movies from API

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
 window.onload = () => {
-    addMovies()
+    // addMovies();
+    fetchMovies();
 }
 
 
-function addMovies(){
+function fetchMovies() {
+    fetch('https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+        addMovies(data);
+    })
+}
+
+
+function addMovies(movies){
     let moviesEl = document.querySelector('.original_movies');
     // <img src="https://wallpapercave.com/wp/wp1934780.jpg" alt="">
-    for(let i = 0;i < 5;i++){
-        moviesEl.innerHTML += '<img src="https://wallpapercave.com/wp/wp1934780.jpg" alt=""></img>'
+    for(let i = 0;i < movies.results.length;i++){
+        moviesEl.innerHTML += movies.results[i]["url(\"backdrop_path\")"];
     }
 }
 
