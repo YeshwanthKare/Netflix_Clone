a4e7bc0d77f60dcd7b9413c9c81e43ab116823e3
YeshwanthKare <yesh.kare@gmail.com>
2021-03-11 22:00:50 +0100

    fetch images

--- script.js
+++ script.js
@@ -21,19 +21,22 @@ function fetchMovies(url, element_selector, path_type) {
     })
     .then((data) => {
         showMovies(data, element_selector, path_type)
+        console.log(data)
     })
     .catch((error) => {
         console.log(error);
-    })
+    }) 
 }
 
 function showMovies(movies, element_selector, path_type){
    var movieEl = document.querySelector(element_selector);
     for(var movie of movies.results){
-       let image = `
-            <img src="https://image.tmdb.org/t/p/original${movie[path_type]}"></img>
-        `
-    movieEl.innerHTML += image;
+        let movieElement = document.createElement("img");
+        movieElement.src =`https://image.tmdb.org/t/p/original${movie[path_type]}`
+        movieElement.addEventListener("click", (e) => {
+            console.log(e)
+        })
+        movieEl.appendChild(movieElement);
     }
 }
 
@@ -46,5 +49,4 @@ function fetchTrending(){
 function fetchTopRated(){
     var url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
     fetchMovies(url, '#top_rated', 'backdrop_path');
-   
 }
