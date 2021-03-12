78d80d7841a284bab259953a250331509f6d8f7d
YeshwanthKare <yesh.kare@gmail.com>
2021-03-12 16:19:56 +0100

    adding youtube feature

--- plan.md
+++ plan.md
@@ -21,3 +21,24 @@
 ~ Horizontal scroll for movies
 ~ Retrieve the other movies from the API
 ~ Add the hoover effect to individual movies
+
+11/03/2021
+
+~ Show trailer for the user when they select a movie
+~ find a way to grab the movie (Youtube, Movie Database API, IMDB)
+~ If we can't find the trailer, we need to tell the user 'can't find the trailer'
+~ Open a modal on click of a movie
+~ Disable user interaction
+~ Show the movie in the modal (Load youtube video)
+~ show background black
+
+
+NEXT UP
+~ Login Logout
+~ Playlists
+
+
+12/03/2021
+
+~ Fix the issue with iframe
+~ Add genres to netflix clone
--- script.js
+++ script.js
@@ -28,13 +28,66 @@ function fetchMovies(url, element_selector, path_type) {
     }) 
 }
 
+
+async function getMovieTrailer(id) {
+    var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
+    return await fetch(url)
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        }else {
+            throw new Error("Something went wrong");
+        }
+    })
+
+}
+
+
+const setTrailers = (trailers) => {
+    const iframe = document.getElementById("movieTrailer");
+    const movieNotFound = document.querySelector(".movieNotFound");
+    if (trailers.length > 0) {
+        movieNotFound.classList.add('d-none');
+        iframe.classList.remove("d-none");
+        iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`
+    }else {
+        iframe.classList.add("d-none");
+        movieNotFound.classList.remove("d-none");
+    }
+}
+
+
+const handleMovieSection = (e) => {
+    const id = e.target.getAttribute('data-id');
+    // console.log(id);
+    const iframe = document.getElementById("movieTrailer");
+
+    getMovieTrailer(id).then((data) => {
+        const results = data.results;
+        // console.log(results)
+        const youTubeTrailers = results.filter((result) => {
+            if(result.site == "YouTube" && result.type == "Trailer"){
+                return true;
+            }else {
+                return false;
+            }
+        })
+
+        setTrailers(youTubeTrailers);
+        
+    })
+    // console.log(e.target);
+    $('#trailer-modal').modal('show');
+}
+
 function showMovies(movies, element_selector, path_type){
    var movieEl = document.querySelector(element_selector);
     for(var movie of movies.results){
         let movieElement = document.createElement("img");
+        movieElement.setAttribute('data-id', movie.id);
         movieElement.src =`https://image.tmdb.org/t/p/original${movie[path_type]}`
         movieElement.addEventListener("click", (e) => {
-            console.log(e)
+            handleMovieSection(e);
         })
         movieEl.appendChild(movieElement);
     }
